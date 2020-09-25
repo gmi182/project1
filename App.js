16788a657c3d61eac2c1f7e29a180c50da3272f4
Gon Inigo <gmi182@gmail.com>
2020-09-25 15:59:37 -0700

    [project1] - Basic Pomodoro functionality implemented

--- App.js
+++ App.js
@@ -2,23 +2,38 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MyTimer from './Timer.js';
 
-export default class App extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Open up App.js to start working on your app!</Text>
-                <MyTimer></MyTimer>
-            </View>
-            
-        );
-    }
-}
-
+const WORK_BG_COLOR = "green";
+const BREAK_BG_COLOR = "blue";
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#FFF',
-        alignItems: 'center',
-        justifyContent: 'center',
+        alignItems: "center",
+        justifyContent: "center",
     },
 });
+
+export default class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            isWork: true,
+            backgroundColor: WORK_BG_COLOR
+        }
+    }
+
+    onTimeTypeChange = isWork => {
+        let bgColor = isWork ? WORK_BG_COLOR : BREAK_BG_COLOR;
+        this.setState({
+            backgroundColor: bgColor,
+            isWork: isWork
+        });
+    }
+
+    render() {
+        return (
+            <View style={[styles.container, {backgroundColor: this.state.backgroundColor}]}>
+                <MyTimer onTypeChange={this.onTimeTypeChange}></MyTimer>
+            </View>
+        );
+    }
+}
--- Timer.js
+++ Timer.js
@@ -1,13 +1,40 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import moment from 'moment';
+import momentDurationFormatSetup from 'moment-duration-format';
+
+const WORK_TITLE = "Work Time";
+const BREAK_TITLE = "Take a Break";
+const WORK_TIME = 2 * 10;
+const BREAK_TIME = 1 * 10;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "transparent",
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 40,
+        fontWeight: "bold"
+    },
+    timer: {
+        fontSize: 24
+    }
+});
+
 
 class MyTimer extends React.Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props);
+        momentDurationFormatSetup(moment);
         this.state = {
-            count: 1,
+            count: WORK_TIME,
             isPaused: false,
-            buttonTitle: "| |"
+            buttonTitle: "| |",
+            isWork: true,
+            title: WORK_TITLE
         }
     }
 
@@ -23,17 +50,34 @@ class MyTimer extends React.Component {
     }
 
     increment() {
+        if (this.state.count === 0) {
+            this.props.onTypeChange(!this.state.isWork);
+            if (this.state.isWork === true) {
+                this.setState(prevState => ({
+                    isWork: !prevState.isWork,
+                    count: BREAK_TIME,
+                    title: BREAK_TITLE
+                }));
+            } else {
+                this.setState(prevState => ({
+                    isWork: !prevState.isWork,
+                    count: WORK_TIME,
+                    title: WORK_TITLE
+                }));
+            }
+        }
         if (this.state.isPaused === false) {
             this.setState(prevState => ({
-                count: prevState.count + 1,
+                count: prevState.count - 1
             }));
         }
     }
 
     render() {
         return (
-            <View>
-                <Text>{this.state.count}</Text>
+            <View style={styles.container}>
+                <Text style={styles.title}>{this.state.title}</Text>
+                <Text style={styles.timer}>{moment.duration(this.state.count, "second").format("mm:ss")}</Text>
                 <Button onPress={() => this.playPauseButton()} title={this.state.buttonTitle}></Button>
             </View>
         )
